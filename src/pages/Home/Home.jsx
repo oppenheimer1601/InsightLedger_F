f8b08e7fd207160ba7d6dbe288d7f083caa5555f
Srijani0506 <tkdutta12321@gmail.com>
2025-03-17 14:43:03 +0530

    search

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,28 @@
-import React, {useContext, useEffect, useState} from 'react'
+import {useContext, useEffect, useState} from 'react'
 import './Home.css'
 import {CoinContext} from '../../context/CoinContext'
+import {Link} from 'react-router-dom'
 
 const Home = () => {
 
   const {allCoin, currency}= useContext(CoinContext);
   const [displayCoin, setDisplayCoin]= useState([]);
+  const [input, setInput]= useState('');
+
+  const inputHandler= (event)=> {
+    setInput(event.target.value);
+    if(event.target.value===""){
+      setDisplayCoin(allCoin);
+    }
+  }
+
+  const searchHandler= async (event)=>{
+    event.preventDefault();
+    const coins= await allCoin.filter((item)=>{
+      return item.name.toLowerCase().includes(input.toLowerCase())
+    })
+    setDisplayCoin(coins);
+  }
 
   useEffect(()=>{
     setDisplayCoin(allCoin);
@@ -16,8 +33,11 @@ const Home = () => {
       <div className="hero">
         <h1>Explore the World's <br/> Leading Crypto Marketplace</h1>
         <p>Join the largest cryptocurrency marketplace and dive into the future of digital assets. Sign up now to trade, invest, and stay ahead in the crypto world.</p>
-        <form>
-            <input type="text" placeholder='Search crypto..'/>
+        <form onSubmit={searchHandler}> 
+            <input onChange={inputHandler} list= 'coinlist' value={input} type="text" placeholder='Search crypto..' required/>
+            <datalist id='coinlist'>
+              {allCoin.map((item, index)=>(<option key={index} value={item.name}/>))}
+            </datalist>
             <button type="submit">Search</button>
         </form>
       </div>
@@ -31,7 +51,7 @@ const Home = () => {
         </div>
         {
           displayCoin.slice(0,10).map((item, index)=>(
-            <div className="table-layout" key={index}>
+            <Link to={`/coin/${item.id}`} className="table-layout" key={index}>
               <p>{item.market_cap_rank}</p>
               <div>
                 <img src={item.image} alt=""/>
@@ -40,7 +60,7 @@ const Home = () => {
             <p>{currency.symbol} {item.current_price.toLocaleString()}</p>
             <p className={item.price_change_percentage_24h>0?"green":"red"}>{Math.floor(item.price_change_percentage_24h*100)/100}</p>
             <p className='market-cap'>{currency.symbol} {item.market_cap.toLocaleString()}</p>
-            </div>
+            </Link>
           ))
         }
       </div>
